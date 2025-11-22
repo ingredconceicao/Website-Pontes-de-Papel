f1730358da159b6e4c5141bc386f5a1edb43cf6d
ingredconceicao <ingriddejesus33@gmail.com>
2025-11-22 20:08:16 -0300

    fix: app.ts

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,5 +1,5 @@
 export const config ={
-    port : process.env.PORT || 3000,
+    port : process.env.PORT || 10000,
     mongo_uri : process.env.MONGO_URI ||'ac-jqnjios-shard-00-00.vwqqvgx.mongodb.net',
     jwt_secret : process.env.JWT_SECRET ||'secret',
     node_env : process.env.NODE_ENV ||'development'
